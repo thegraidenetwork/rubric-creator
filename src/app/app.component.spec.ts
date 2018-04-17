c2fc21158d91218806ccc0a2c1391b4ff575ee3b
Karl Hughes <khughes.me@gmail.com>
2018-04-16 20:22:20 -0500

    Updating tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,40 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-xdescribe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+        const successfulCompileCallback = (): void => {
+            fixture = TestBed.createComponent(AppComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+        };
+
+        const failedCompileCallback = (): void => {
+            throw new Error('AppComponent could not be compiled.');
+        };
+
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent,
+            ],
+            imports: [
+                RouterTestingModule,
+            ],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA,
+            ]
+        }).compileComponents().then(
+            successfulCompileCallback,
+            failedCompileCallback
+        );
+    }));
+
+    it('should create app', () => {
+        expect(component).toBeTruthy();
+    });
 });
