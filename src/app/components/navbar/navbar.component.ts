5223b0ad472e8b861057a7f0061a90667ce89584
Karl Hughes <khughes.me@gmail.com>
2018-04-20 23:08:23 -0500

    Cleaning up styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'rc-navbar',
+    styleUrls: ['./navbar.component.css'],
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+    public navbarCollapsed: boolean = true;
 }
