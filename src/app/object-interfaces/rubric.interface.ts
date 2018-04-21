8a20eceb39af5859ed14693fd1637650fbe7f0e2
Karl Hughes <khughes.me@gmail.com>
2018-04-20 19:54:55 -0500

    Updating styles on list page

--- src/app/object-interfaces/rubric.interface.ts
+++ src/app/object-interfaces/rubric.interface.ts
@@ -2,6 +2,7 @@ import { ComponentInterface } from './component.interface';
 
 export interface RubricInterface {
     components?: Array<ComponentInterface>;
+    created_at?: string;
     description?: string;
     maxLevelsCount?: number;
     name: string;
