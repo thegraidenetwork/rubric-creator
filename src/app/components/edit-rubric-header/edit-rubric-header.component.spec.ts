51cae5edd9490fce340bb817a26b1859d05495a0
Karl Hughes <khughes.me@gmail.com>
2018-05-05 20:17:02 -0500

    Updating component tests

--- src/app/components/edit-rubric-header/edit-rubric-header.component.spec.ts
+++ src/app/components/edit-rubric-header/edit-rubric-header.component.spec.ts
@@ -2,13 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditRubricHeaderComponent } from './edit-rubric-header.component';
 import { FormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { rubricsReducer } from '../../store/rubrics.reducer';
-import { getInitialState } from '../../store/rubrics.state';
+import { getInitialState, RubricsStateInterface } from '../../store/rubrics.state';
+import * as faker from 'faker';
+import { RubricInterface } from '../../object-interfaces/rubric.interface';
+import { GetRubricSuccess } from '../../store/rubrics.actions';
 
 describe('EditRubricHeaderComponent', () => {
     let component: EditRubricHeaderComponent;
     let fixture: ComponentFixture<EditRubricHeaderComponent>;
+    let store: Store<RubricsStateInterface>;
 
     beforeEach(async(() => {
         void TestBed.configureTestingModule({
@@ -22,15 +26,30 @@ describe('EditRubricHeaderComponent', () => {
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+
+        store = TestBed.get(Store);
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(EditRubricHeaderComponent);
         component = fixture.componentInstance;
+
+        const rubric: RubricInterface = {
+            description: faker.lorem.words(),
+            name: faker.lorem.words(),
+            uuid: faker.random.uuid(),
+        };
+        const action = new GetRubricSuccess(rubric);
+        store.dispatch(action);
+
         fixture.detectChanges();
     });
 
-    xit('should create', () => {
-        expect(component).toBeTruthy();
+    it('should update rubric via store when called', () => {
+        spyOn(store, 'dispatch').and.callThrough();
+
+        component.updateRubric();
+
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
     });
 });
--- src/app/components/edit-rubric-save-button/edit-rubric-save-button.component.spec.ts
+++ src/app/components/edit-rubric-save-button/edit-rubric-save-button.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditRubricSaveButtonComponent } from './edit-rubric-save-button.component';
 import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { rubricsReducer } from '../../store/rubrics.reducer';
-import { getInitialState } from '../../store/rubrics.state';
+import { getInitialState, RubricsStateInterface } from '../../store/rubrics.state';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import * as faker from 'faker';
+import { RubricInterface } from '../../object-interfaces/rubric.interface';
+import { GetRubricSuccess } from '../../store/rubrics.actions';
 
 describe('EditRubricSaveButtonComponent', () => {
     let component: EditRubricSaveButtonComponent;
     let fixture: ComponentFixture<EditRubricSaveButtonComponent>;
+    let store: Store<RubricsStateInterface>;
 
     beforeEach(async(() => {
         void TestBed.configureTestingModule({
@@ -22,15 +26,67 @@ describe('EditRubricSaveButtonComponent', () => {
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+
+        store = TestBed.get(Store);
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(EditRubricSaveButtonComponent);
         component = fixture.componentInstance;
+
+        const rubric: RubricInterface = {
+            description: faker.lorem.words(),
+            name: faker.lorem.words(),
+            uuid: faker.random.uuid(),
+        };
+        const action = new GetRubricSuccess(rubric);
+        store.dispatch(action);
+
         fixture.detectChanges();
     });
 
-    xit('should create', () => {
-        expect(component).toBeTruthy();
+    it('should get save button text when saving', () => {
+        component.saving = false;
+
+        expect(component.saveButtonText).toBe('Save');
+    });
+
+    it('should get save button text when not saving', () => {
+        component.saving = true;
+
+        expect(component.saveButtonText).toBe('Saving');
+    });
+
+    it('should return false when rubric is invalid', () => {
+        component.rubric = {
+            name: faker.lorem.words(),
+        };
+
+        expect(component.isValid()).toBe(false);
+    });
+
+    it('should return true when rubric is valid', () => {
+        component.rubric = {
+            components: [
+                {
+                    levels: [
+                        {score: faker.random.number()},
+                        {score: faker.random.number()},
+                    ],
+                    name: faker.lorem.words(),
+                },
+            ],
+            name: faker.lorem.words(),
+        };
+
+        expect(component.isValid()).toBe(true);
+    });
+
+    it('should create rubric via store when called', () => {
+        spyOn(store, 'dispatch').and.callThrough();
+
+        component.saveRubric();
+
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
     });
 });
