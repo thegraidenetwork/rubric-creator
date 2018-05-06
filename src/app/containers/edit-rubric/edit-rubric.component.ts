7227c2097be5816cd1b44052f3575b2a40b6cee5
Karl Hughes <khughes.me@gmail.com>
2018-05-06 07:38:51 -0500

    Adding breadcrumbs for consistency

--- src/app/containers/edit-rubric/edit-rubric.component.ts
+++ src/app/containers/edit-rubric/edit-rubric.component.ts
@@ -2,19 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { BaseRubricComponent } from '../../components/base/base-rubric.component';
 import { Store } from '@ngrx/store';
 import { RubricsStateInterface } from '../../store/rubrics.state';
-import { GetRubric } from '../../store/rubrics.actions';
+import { GetRubric, SetBreadcrumbs } from '../../store/rubrics.actions';
+import { BreadcrumbInterface } from '../../object-interfaces/breadcrumb.interface';
 
 @Component({
     selector: 'rc-create-rubric',
     templateUrl: './edit-rubric.component.html',
 })
 export class EditRubricComponent extends BaseRubricComponent implements OnInit {
+    private breadcrumbs: Array<BreadcrumbInterface> = [
+        {
+            path: '/',
+            selected: false,
+            text: 'Home',
+        },
+        {
+            path: '/rubrics',
+            selected: false,
+            text: 'Rubrics',
+        },
+        {
+            path: undefined,
+            selected: true,
+            text: 'Create',
+        },
+    ];
 
     constructor(protected store: Store<RubricsStateInterface>) {
         super(store);
     }
 
     public ngOnInit(): void {
+        this.store.dispatch(new SetBreadcrumbs(this.breadcrumbs));
         this.store.dispatch(new GetRubric());
         super.ngOnInit();
     }
