2200ed7fdd972ad9f27130a11bfc0a49c61487d1
Karl Hughes <khughes.me@gmail.com>
2018-05-09 09:53:13 -0500

    Updating data array

--- src/app/data-services/data/default-rubrics.array.ts
+++ src/app/data-services/data/default-rubrics.array.ts
@@ -2,28 +2,28 @@ import { RubricInterface } from '../../object-interfaces/rubric.interface';
 
 export const defaultRubricsArray: Array<RubricInterface> = [
     {
-        uuid: '5af0bf130fb4d74cdf23d79f/1',
         created_at: '2018-05-01 19:11:10',
         name: 'AP Psychology Final - Free Response Questions',
+        uuid: '5af0bf130fb4d74cdf23d79f/1',
     },
     {
-        uuid: '5af0c23d7a973f4ce57837b4',
         created_at: '2018-04-25 20:04:23',
         name: 'AP Literature - 2017 Question 3',
+        uuid: '5af0c23d7a973f4ce57837b4',
     },
     {
-        uuid: '5af0c2b8c83f6d4cc7348fb5',
         created_at: '2018-04-25 18:31:40',
         name: 'AP Language - 2014 Question 2',
+        uuid: '5af0c2b8c83f6d4cc7348fb5',
     },
     {
-        uuid: '5af0c3057a973f4ce57837b6',
         created_at: '2018-04-26 19:33:28',
         name: 'Sophomore English Research Project',
+        uuid: '5af0c3057a973f4ce57837b6',
     },
     {
-        uuid: '5af0c34fc83f6d4cc7348fb7',
         created_at: '2018-04-13 14:11:41',
         name: 'High School English - Persuasive Writing',
+        uuid: '5af0c34fc83f6d4cc7348fb7',
     },
 ];
