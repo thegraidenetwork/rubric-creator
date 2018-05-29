8584085a34f33e8f9864c3106b29b008264965e3
Karl Hughes <khughes.me@gmail.com>
2018-05-29 10:00:27 -0500

    Moving more vars to .env

--- docker/execute.sh
+++ docker/execute.sh
@@ -6,9 +6,9 @@ echo "Files to transfer:"
 ls /dist
 
 echo "Transferring:"
-az storage blob upload-batch -d http://tgncdnstorage.blob.core.windows.net/$AZURE_CONTAINER_NAME -s /dist --content-cache-control "$AZURE_CACHE_CONTROL" --account-key $AZURE_STORAGE_ACCOUNT_KEY --account-name $AZURE_STORAGE_ACCOUNT_NAME
+az storage blob upload-batch -d $AZURE_CONTAINER_PATH -s /dist --content-cache-control "$AZURE_CACHE_CONTROL" --account-key $AZURE_STORAGE_ACCOUNT_KEY --account-name $AZURE_STORAGE_ACCOUNT_NAME
 
 # echo "Purging cache:"
-# az cdn endpoint purge -n $AZURE_ENDPOINT_NAME --content-paths / --profile-name tgn-cdn --resource-group group-production
+az cdn endpoint purge -n $AZURE_ENDPOINT_NAME --content-paths / --profile-name $AZURE_CDN_PROFILE_NAME --resource-group $AZURE_CDN_RESOURCE_GROUP
 
 echo "CDN Deployment complete."
