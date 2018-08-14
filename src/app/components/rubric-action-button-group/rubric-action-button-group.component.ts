762c51b63c434625fbb438e6f58d5484882c670c
Karl Hughes <khughes.me@gmail.com>
2018-08-14 16:36:25 -0500

    Adding copy link

--- src/app/components/rubric-action-button-group/rubric-action-button-group.component.ts
+++ src/app/components/rubric-action-button-group/rubric-action-button-group.component.ts
@@ -12,11 +12,24 @@ export class RubricActionButtonGroupComponent {
     @Input() public floatRight: boolean = false;
     @Input() public rubric: RubricInterface;
     @Input() public showPrint: boolean = false;
+    public copied: boolean = false;
+    private baseUrl: string = 'https://www.rubriccreator.com';
 
     constructor(private modalService: NgbModal) {}
 
     public copyLink(): false {
-        console.log('Copy link');
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = `${this.baseUrl}/rubrics/${this.rubric.uuid}`;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(selBox);
+        this.copied = true;
 
         return false;
     }
