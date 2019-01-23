f7be045a47aeaef235c4c611942c62826408d88d
Hollie Lambert <hollielambert@Hollies-MacBook-Pro.local>
2019-01-23 15:34:44 -0600

    Refactor test cases

--- src/app/functions/get-max-levels-from-rubric.function.spec.ts
+++ src/app/functions/get-max-levels-from-rubric.function.spec.ts
@@ -2,33 +2,63 @@ import * as faker from 'faker';
 import { RubricInterface } from '../object-interfaces/rubric.interface';
 import { getMaxLevelsFromRubric } from './get-max-levels-from-rubric.function';
 
-fdescribe('Function: getMaxLevelsFromRubric', () => {
-    fit('should count max levels in rubric', () => {
-        // generate a random number between 1 - 20
-        const randomNumber = () => faker.random.number({ min: 1, max: 20 });
-        
-        // generate an array that has a random number of objects
-        const generateArray = () => new Array(randomNumber()).fill({});
+describe('Function: getMaxLevelsFromRubric', () => {
+    it('should count max levels in rubric', () => {
 
-        // generate an array with a random number of objects with score keys of random numbers
-        const generateLevels = () => generateArray().map(() => {
-            return { score: faker.random.number() }
-        });
+    const maxLevels = faker.random.number({ min: 1, max: 20 });
 
-        // map over each component, filling in levels and names
-        const components = generateArray().map(() => {
-            return { levels: generateLevels(), name: faker.random.word() };
-        });
+        const generateComponents = (maxLevelsCount: number) => {
+            const randomNumber = (max: number = 20) => faker.random.number({ min: 1, max });
+            
+            const generateArray = (maxElements?: number) => {
+                let randomElements = randomNumber();
+                if (maxElements !== undefined) {
+                    randomElements = randomNumber(maxElements);
+                }
+                return new Array(randomElements).fill({})
+            };
+
+            const generateLevels = () => generateArray(maxLevelsCount).map(() => {
+                return { score: faker.random.number() }
+            });
+
+            return generateArray().map(() => {
+                return { levels: generateLevels(), name: faker.random.word() };
+            });
+        }
 
         const rubric: RubricInterface = {
-            components,
+            components: generateComponents(maxLevels),
             name: faker.lorem.words(),
         };
 
-        console.log(rubric);
+        const result = getMaxLevelsFromRubric(rubric);
+
+        expect(result).toBeLessThanOrEqual(maxLevels);
+    });
+
+    it('should return zero if rubric components are undefined', () => {
+
+        const rubric: RubricInterface = {
+            components: undefined,
+            name: faker.lorem.words(),
+        }
 
         const result = getMaxLevelsFromRubric(rubric);
 
-        expect(result).toBe(3);
+        expect(result).toEqual(0);
+    })
+
+    it('should return zero if rubric components are empty', () => {
+
+        const rubric: RubricInterface = {
+            components: [],
+            name: faker.lorem.words(),
+        }
+
+        const result = getMaxLevelsFromRubric(rubric);
+
+        expect(result).toEqual(0);
     });
+
 });
