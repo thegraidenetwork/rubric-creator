8c04fe1a6f355502a971724808f10fb9982abb56
Hollie Lambert <hollielambert@hollies-mbp.attlocal.net>
2019-01-26 13:38:01 -0600

    Reorganized tests

--- src/app/store/rubrics.reducer.spec.ts
+++ src/app/store/rubrics.reducer.spec.ts
@@ -1,233 +1,258 @@
-import { rubricsReducer } from './rubrics.reducer';
+import { DisplayableErrorInterface } from '../object-interfaces/displayable-error.interface';
+import { RubricInterface } from '../object-interfaces/rubric.interface';
 import {
-  ConnectionLost,
-  ConnectionMade,
-  CreateRubric,
-  CreateRubricError,
-  CreateRubricSuccess,
-  DeleteRubric,
-  DeleteRubricError,
-  DeleteRubricSuccess,
-  GetRubric,
-  GetRubricError,
-  GetRubrics,
-  GetRubricsError,
-  GetRubricsSuccess,
-  GetRubricSuccess,
-  SetPageTitle,
-  UpdateCurrentRubric,
+    ConnectionLost,
+    ConnectionMade,
+    CreateRubric,
+    CreateRubricError,
+    CreateRubricSuccess,
+    DeleteRubric,
+    DeleteRubricError,
+    DeleteRubricSuccess,
+    GetRubric,
+    GetRubricError,
+    GetRubrics,
+    GetRubricsError,
+    GetRubricsSuccess,
+    GetRubricSuccess,
+    SetPageTitle,
+    UpdateCurrentRubric,
 } from './rubrics.actions';
+import { rubricsReducer } from './rubrics.reducer';
 import { getInitialState } from './rubrics.state';
 
-fdescribe('RubricsReducer', () => {
-  describe('GetRubric action', () => {
-    it('should set currentRubric to undefined', () => {
-      const { rubrics: initialState } = getInitialState();
-      const action = new GetRubric('test');
+describe('RubricsReducer', () => {
+    const { rubrics: initialState } = getInitialState();
+    const rubric: RubricInterface = { name: 'test' };
+    const currentRubric: RubricInterface = { maxLevelsCount: 0, name: 'test', totalPoints: 0 };
+    const allRubrics: Array<RubricInterface> = [];
+    const error: DisplayableErrorInterface = { message: 'test' };
 
-      const state = rubricsReducer(initialState, action);
+    describe('Undefined action', () => {
+        it('should return the default state', () => {
+            const action = { type: 'undefined' } as any;
 
-      void expect(state.currentRubric).toEqual(undefined);
+            const result = rubricsReducer(initialState, action);
+
+            void expect(result).toBe(initialState);
+        });
     });
-  });
 
-  describe('GetRubricSuccess action', () => {
-    it('should set currentRubric to the correct rubric', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubric = { maxLevelsCount: 0, name: 'test', totalPoints: 0 };
-      const action = new GetRubricSuccess(rubric);
+    describe('GetRubricSuccess action', () => {
+        it('should set currentRubric to rubric', () => {
+            const action = new GetRubricSuccess(rubric);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(rubric);
+            void expect(result).toEqual({
+                ...initialState,
+                currentRubric,
+                error: undefined,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('GetRubricError action', () => {
-    it('should set an error message', () => {
-      const { rubrics: initialState } = getInitialState();
-      const error = { message: 'test' };
-      const action = new GetRubricError(error);
+    describe('UpdateCurrentRubric action', () => {
+        it('should set currentRubric to rubric', () => {
+            const action = new UpdateCurrentRubric(rubric);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(undefined);
-      void expect(state.allRubrics).toEqual(undefined);
-      void expect(state.error).toEqual(error);
-      void expect(state.saving).toEqual(false);
-      void expect(state.deleting).toEqual(false);
+            void expect(result).toEqual({
+                ...initialState,
+                currentRubric,
+                error: undefined,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('GetRubrics action', () => {
-    it('should return the default state', () => {
-      const { rubrics: initialState } = getInitialState();
-      const action = new GetRubrics();
+    describe('CreateRubricSuccess action', () => {
+        it('should set currentRubric to rubric', () => {
+            const action = new CreateRubricSuccess(rubric);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state).toEqual(initialState);
+            void expect(result).toEqual({
+                ...initialState,
+                currentRubric,
+                error: undefined,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('GetRubricsSuccess action', () => {
-    it('should set allRubrics to an array of rubrics', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubrics = [];
-      const action = new GetRubricsSuccess(rubrics);
+    describe('GetRubricsSuccess action', () => {
+        it('should set allRubrics to array of rubrics', () => {
+            const action = new GetRubricsSuccess(allRubrics);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.allRubrics).toEqual(rubrics);
-      void expect(state.error).toEqual(undefined);
+            void expect(result).toEqual({
+                ...initialState,
+                allRubrics,
+                deleting: false,
+                error: undefined,
+            });
+        });
     });
-  });
 
-  describe('GetRubricsError action', () => {
-    it('should set an error message', () => {
-      const { rubrics: initialState } = getInitialState();
-      const error = { message: 'test' };
-      const action = new GetRubricsError(error);
+    describe('DeleteRubricSuccess action', () => {
+        it('should set deleting to false', () => {
+            const action = new DeleteRubricSuccess(allRubrics);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(undefined);
-      void expect(state.allRubrics).toEqual(undefined);
-      void expect(state.error).toEqual(error);
-      void expect(state.saving).toEqual(false);
-      void expect(state.deleting).toEqual(false);
+            void expect(result).toEqual({
+                ...initialState,
+                allRubrics,
+                deleting: false,
+                error: undefined,
+            });
+        });
     });
-  });
 
-  describe('DeleteRubric action', () => {
-    it('should set deleting to true', () => {
-      const { rubrics: initialState } = getInitialState();
-      const action = new DeleteRubric();
+    describe('CreateRubricError action', () => {
+        it('should set an error message', () => {
+            const action = new CreateRubricError(error);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(undefined);
-      void expect(state.deleting).toEqual(true);
+            void expect(result).toEqual({
+                ...initialState,
+                error,
+                allRubrics: undefined,
+                currentRubric: undefined,
+                deleting: false,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('DeleteRubricSuccess action', () => {
-    it('should set deleting to true', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubrics = [];
-      const action = new DeleteRubricSuccess(rubrics);
+    describe('GetRubricError action', () => {
+        it('should set an error message', () => {
+            const action = new GetRubricError(error);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.allRubrics).toEqual(rubrics);
-      void expect(state.error).toEqual(undefined);
-      void expect(state.deleting).toEqual(false);
+            void expect(result).toEqual({
+                ...initialState,
+                error,
+                allRubrics: undefined,
+                currentRubric: undefined,
+                deleting: false,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('DeleteRubricError action', () => {
-    it('should set an error message', () => {
-      const { rubrics: initialState } = getInitialState();
-      const error = { message: 'test' };
-      const action = new DeleteRubricError(error);
+    describe('GetRubricsError action', () => {
+        it('should set an error message', () => {
+            const action = new GetRubricsError(error);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(undefined);
-      void expect(state.allRubrics).toEqual(undefined);
-      void expect(state.error).toEqual(error);
-      void expect(state.saving).toEqual(false);
-      void expect(state.deleting).toEqual(false);
+            void expect(result).toEqual({
+                ...initialState,
+                error,
+                allRubrics: undefined,
+                currentRubric: undefined,
+                deleting: false,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('UpdateCurrentRubric action', () => {
-    it('should set currentRubric to the correct rubric', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubric = { maxLevelsCount: 0, name: 'test', totalPoints: 0 };
-      const action = new UpdateCurrentRubric(rubric);
+    describe('DeleteRubricError action', () => {
+        it('should set an error message', () => {
+            const action = new DeleteRubricError(error);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(rubric);
-      void expect(state.error).toEqual(undefined);
-      void expect(state.saving).toEqual(false);
+            void expect(result).toEqual({
+                ...initialState,
+                error,
+                allRubrics: undefined,
+                currentRubric: undefined,
+                deleting: false,
+                saving: false,
+            });
+        });
     });
-  });
 
-  describe('CreateRubric action', () => {
-    it('should set saving to true', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubric = { maxLevelsCount: 0, name: 'test', totalPoints: 0 };
-      const action = new CreateRubric(rubric);
+    describe('DeleteRubric action', () => {
+        it('should set deleting to true', () => {
+            const action = new DeleteRubric();
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.saving).toEqual(true);
+            void void expect(result).toEqual({
+                ...initialState,
+                currentRubric: undefined,
+                deleting: true,
+            });
+        });
     });
-  });
 
-  describe('CreateRubricSuccess action', () => {
-    it('should set currentRubric to the correct rubric', () => {
-      const { rubrics: initialState } = getInitialState();
-      const rubric = { maxLevelsCount: 0, name: 'test', totalPoints: 0 };
-      const action = new CreateRubricSuccess(rubric);
+    describe('GetRubric action', () => {
+        it('should set currentRubric to undefined', () => {
+            const action = new GetRubric('test');
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(rubric);
-      void expect(state.error).toEqual(undefined);
-      void expect(state.saving).toEqual(false);
+            void expect(result).toEqual({ ...initialState, currentRubric: undefined });
+        });
     });
-  });
 
-  describe('CreateRubricError action', () => {
-    it('should set an error message', () => {
-      const { rubrics: initialState } = getInitialState();
-      const error = { message: 'test' };
-      const action = new CreateRubricError(error);
+    describe('SetPageTitle action', () => {
+        it('should set pageTitle to title', () => {
+            const pageTitle = 'test';
+            const action = new SetPageTitle(pageTitle);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.currentRubric).toEqual(undefined);
-      void expect(state.allRubrics).toEqual(undefined);
-      void expect(state.error).toEqual(error);
-      void expect(state.saving).toEqual(false);
-      void expect(state.deleting).toEqual(false);
+            void expect(result).toEqual({ ...initialState, pageTitle });
+        });
     });
-  });
 
-  describe('SetPageTitle action', () => {
-    it('should set the pageTitle to the correct title', () => {
-      const { rubrics: initialState } = getInitialState();
-      const title = 'test';
-      const action = new SetPageTitle(title);
+    describe('CreateRubric action', () => {
+        it('should set saving to true', () => {
+            const action = new CreateRubric(rubric);
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.pageTitle).toEqual(title);
+            void expect(result).toEqual({ ...initialState, saving: true });
+        });
     });
-  });
 
-  describe('ConnectionMade action', () => {
-    it('should set connected to true', () => {
-      const { rubrics: initialState } = getInitialState();
-      const action = new ConnectionMade();
+    describe('ConnectionMade action', () => {
+        it('should set connected to true', () => {
+            const action = new ConnectionMade();
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.connected).toEqual(true);
+            void expect(result).toEqual({ ...initialState, connected: true });
+        });
     });
-  });
 
-  describe('ConnectionLost action', () => {
-    it('should set connected to true', () => {
-      const { rubrics: initialState } = getInitialState();
-      const action = new ConnectionLost();
+    describe('ConnectionLost action', () => {
+        it('should set connected to true', () => {
+            const action = new ConnectionLost();
 
-      const state = rubricsReducer(initialState, action);
+            const result = rubricsReducer(initialState, action);
 
-      void expect(state.connected).toEqual(false);
+            void expect(result).toEqual({ ...initialState, connected: false });
+        });
     });
-  });
+
+    describe('GetRubrics action', () => {
+        it('should return the default state', () => {
+            const action = new GetRubrics();
+
+            const result = rubricsReducer(initialState, action);
+
+            void expect(result).toEqual(initialState);
+        });
+    });
 });
