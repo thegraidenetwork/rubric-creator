e606820dc9ca9afd5467d85aea83a1f6bb2c26cc
Hollie Lambert <hollielambert@Hollies-MacBook-Pro.local>
2019-01-29 12:56:32 -0600

    Adding tests

--- src/app/components/rubric-action-button-group/rubric-action-button-group.component.spec.ts
+++ src/app/components/rubric-action-button-group/rubric-action-button-group.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RubricActionButtonGroupComponent } from './rubric-action-button-group.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import * as faker from 'faker';
 
@@ -33,9 +33,45 @@ describe('RubricActionButtonGroupComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should render buttons', () => {
+    it('should render 2 buttons when its parent is list-item', () => {
+        component.floatRight = true;
+
         const buttons = fixture.nativeElement.querySelectorAll('button');
-        void expect(buttons.length).toBeGreaterThanOrEqual(2);
-        void expect(buttons.length).toBeLessThanOrEqual(4);
+
+        void expect(buttons.length).toEqual(2);
+    });
+
+    it('should render 3 buttons when its parent is rubric-header', () => {
+        component.showPrint = true;
+        fixture.detectChanges();
+
+        const buttons = fixture.nativeElement.querySelectorAll('button');
+
+        void expect(buttons.length).toEqual(3);
+    });
+
+    it('should set copied to true when copyLink is called', () => {
+        component.copyLink();
+
+        void expect(component.copied).toEqual(true);
+    });
+
+    it('should call window.print when printRubric is called', () => {
+        spyOn(window, 'print');
+
+        component.printRubric();
+
+        void expect(window.print).toHaveBeenCalled();
+    });
+
+    it('should call openDeleteRubricModal when Delete Rubric button clicked', () => {
+        component.rubric.private = true;
+        fixture.detectChanges();
+        spyOn(component, 'openDeleteRubricModal');
+        const button = fixture.debugElement.nativeElement.querySelector('#btn-delete');
+
+        button.click();
+
+        void expect(component.openDeleteRubricModal).toHaveBeenCalled();
     });
 });
