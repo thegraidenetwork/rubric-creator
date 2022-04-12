919a4ad962c0e2611ffcb998b8fed6f102513236
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-04-12 14:48:34 -0500

    Add overwrite param for new azure version

--- docker/execute.sh
+++ docker/execute.sh
@@ -6,7 +6,7 @@ echo "Files to transfer:"
 ls /dist
 
 echo "Transferring:"
-az storage blob upload-batch -d $AZURE_CONTAINER_PATH -s /dist --content-cache-control "$AZURE_CACHE_CONTROL" --account-key $AZURE_STORAGE_ACCOUNT_KEY --account-name $AZURE_STORAGE_ACCOUNT_NAME
+az storage blob upload-batch -d $AZURE_CONTAINER_PATH -s /dist --content-cache-control "$AZURE_CACHE_CONTROL" --account-key $AZURE_STORAGE_ACCOUNT_KEY --account-name $AZURE_STORAGE_ACCOUNT_NAME --overwrite
 
 echo "Purging cache:"
 az cdn endpoint purge -n $AZURE_ENDPOINT_NAME --content-paths / --profile-name $AZURE_CDN_PROFILE_NAME --resource-group $AZURE_CDN_RESOURCE_GROUP
